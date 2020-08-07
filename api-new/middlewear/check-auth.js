8e82537991aadded3ce4d29961dd05db14fee8e0
dreamgirlto <38843981+dreamgirlto@users.noreply.github.com>
2020-08-06 21:11:46 -0400

    changed logged out redirect from /home to /

--- api-new/middlewear/check-auth.js
+++ api-new/middlewear/check-auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 module.exports = async (req, res, next) => {
     console.log('verify running')
     try {
-        // This line will have to change. Instead of req.headers, we'll have to look in res.cookies. 
         const token = req.cookies.access_token;
         console.log(token)
         const decoded = jwt.verify(token, process.env.JWT_KEY)
